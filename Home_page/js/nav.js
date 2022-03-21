ffa37b818e9006a528383a60abdb648159ec2901
paravoke <66128724+paravoke@users.noreply.github.com>
2022-03-22 00:53:10 +0100

    Latest version of home page

---
+++ Home_page/js/nav.js
@@ -0,0 +1,14 @@
+let k=0;
+function disp(){ 
+if ((k % 2)==0) {
+document.getElementById("navShow").style.display = "grid";
+k++;
+
+}
+ else {
+	document.getElementById("navShow").style.display = "none";
+	k++;
+
+}
+
+}

d4ea7cf26002ee80d2e6cb8e669d3f4373c5c397
paravoke <66128724+paravoke@users.noreply.github.com>
2022-03-21 22:59:26 +0100

    Page d'acceuil Jinx_art v1
    i was able to do more intersting things but the time is saif

---
+++ js.js
@@ -0,0 +1,14 @@
+let k=0;
+function disp(){ 
+if ((k % 2)==0) {
+document.getElementById("izzan").style.display = "grid";
+k++;
+
+}
+ else {
+	document.getElementById("izzan").style.display = "none";
+	k++;
+
+}
+
+}
